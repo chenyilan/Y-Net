2dfbc99daf8caf1211b97a91d5a37beecec231c5
chenyilan <516730917@qq.com>
2019-07-13 14:04:29 +0800

    Create __init__.py

---
+++ networks/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name:       __init__
+   Project Name:    bf enhancement
+   Author :         Hengrong Lan
+   Date:            2019/1/1
+-------------------------------------------------
+   Change Activity:
+                   2019/1/1:
+-------------------------------------------------
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
