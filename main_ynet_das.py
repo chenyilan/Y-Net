a1657e4f7a74ff5a7f969c98c9bf242c57cfd16c
chenyilan <516730917@qq.com>
2020-07-03 15:16:55 +0800

    Add files via upload
    deleted some visualization to keep the program running.

--- main_ynet_das.py
+++ main_ynet_das.py
@@ -13,10 +13,9 @@
 """
 
 from networks.model_ynet import YNet
-from utils.visualizer import Visualizer
+
 from skimage.measure import compare_ssim, compare_psnr
 from utils.mydataset import ReconDataset
-from utils.trick import *
 import os
 import time
 import random
@@ -37,25 +36,11 @@ start_epoch=0
 loadcp = False
 
 
-losses = AverageMeter()
-batch_time = AverageMeter()
-data_time = AverageMeter()
-train_ssim_meter = AverageMeter()
-train_psnr_meter = AverageMeter()
-train_ssim_top20 = AverageMeter(num_top=20)
-train_psnr_top20 = AverageMeter(num_top=20)
-test_ssim_meter = AverageMeter()
-test_psnr_meter = AverageMeter()
-test_ssim_top10 = AverageMeter(num_top=10)
-test_psnr_top10 = AverageMeter(num_top=10)
-
-losses_list_avg=[]
-losses_list_val=[]
 
 
 curr_lr = learning_rate
 
-vis = Visualizer(env='beam_ynet_3cat')
+
 
 
 #source activate pytorch
@@ -125,41 +110,19 @@ while True:
                 optimizer.step()
                 
                 ssim = compare_ssim(np.array(reimage.detach().squeeze()), np.array(outputs.detach().squeeze()))
-                train_ssim_meter.update(ssim)
-                psnr = compare_psnr(np.array(reimage.detach().squeeze()), np.array(outputs.detach().squeeze()), data_range=255)
-                train_psnr_meter.update(psnr)
-                
-                # visualization and evaluation
-                if (batch_idx +1) %8 ==0:
-                   out_image= outputs.detach()
-                   bf_img= bfimg.detach()
-                   vis_index= random.randint(0,reimage.size(0)-1)
-                   vis.img(name='ground truth',img_=10*reimage[vis_index])
-                   vis.img(name='beamform', img_=30*bf_img[vis_index])
-                   vis.img(name='reconstruction', img_=10*out_image[vis_index])
 
-                batch_time.update(time.time() - end)
+                psnr = compare_psnr(np.array(reimage.detach().squeeze()), np.array(outputs.detach().squeeze()), data_range=255)
+
+                
+
+
+                batch_time=(time.time() - end)
                 end = time.time()
 
                 if (batch_idx + 1) % 20 == 0:
-                        print('Epoch [{}], Start [{}], Step [{}/{}], Loss: {:.4f},Time [{batch_time.val:.3f}({batch_time.avg:.3f})]'
-                              .format(epoch + 1, start_epoch, batch_idx + 1, total_step, loss.item(),batch_time=batch_time))
+                        print('Epoch [{}], Start [{}], Step [{}/{}], Loss: {:.4f},Time:{:/4f}}]'
+                              .format(epoch + 1, start_epoch, batch_idx + 1, total_step, loss.item(),batch_time))
 
-        vis.plot_multi_win(
-            dict(
-                loss_val=losses.val,
-                loss_avg=losses.avg)) 
-        losses_list_avg.append(losses.val)
-        losses_list_val.append(losses.avg)
-        
-        vis.plot_multi_win(dict(
-                                train_ssim=train_ssim_meter.avg, 
-                                train_psnr=train_psnr_meter.avg))  
-        mean, deviation, best = train_ssim_top20.top_update_calc(train_ssim_meter.avg)
-        vis.plot_single_win(dict(mean=mean, deviation=deviation, best=best), win='train_ssim_')
-        
-        mean, deviation, best = train_psnr_top20.top_update_calc(train_psnr_meter.avg)
-        vis.plot_single_win(dict(mean=mean, deviation=deviation, best=best), win='train_psnr_')
         
         # Validata
         if (epoch + 1) % 10 == 0:
@@ -171,27 +134,10 @@ while True:
                    bfimg = F.upsample(bfimg, (128, 128), mode='bilinear')
                    outputs = model(rawdata,bfimg)  
                    ssim = compare_ssim(np.array(reimage.squeeze()), np.array(outputs.squeeze()))
-                   test_ssim_meter.update(ssim)
+
                    psnr = compare_psnr(np.array(reimage.squeeze()), np.array(outputs.squeeze()), data_range=255)
-                   test_psnr_meter.update(psnr) 
-                   if (batch_idx +1) % 10 ==0:
-                      out_image= outputs.detach()   
-                      bf_img= bfimg.detach()                   
-                      vis_index= random.randint(0,reimage.size(0)-1)
-                      vis.img(name='Test: ground truth',img_=10*reimage[vis_index])
-                      vis.img(name='Test: beamform', img_=30*bf_img[vis_index])
-                      vis.img(name='Test: reconstruction', img_=10*out_image[vis_index]) 
-                      
-               
-               vis.plot_multi_win(dict(
-                                      test_ssim=test_ssim_meter.avg, 
-                                      test_psnr=test_psnr_meter.avg))  
-               mean, deviation, best = test_ssim_top10.top_update_calc(test_ssim_meter.avg)
-               vis.plot_single_win(dict(mean=mean, deviation=deviation, best=best), win='test_ssim_')
-        
-               mean, deviation, best = test_psnr_top10.top_update_calc(test_psnr_meter.avg)
-               vis.plot_single_win(dict(mean=mean, deviation=deviation, best=best), win='test_psnr_')
-         
+
+
         # Decay learning rate
         if (epoch + 1) % 50 == 0:
                 curr_lr /= 5
